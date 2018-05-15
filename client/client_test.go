turbinelabs/api

api/http: flag for custom headers (#1857)
api/http: push net/http.Client into Endpoint
nonstdlib/flag: allow multiple flags to set the list
api/client/flags: validate custom headers
api/http: RequestHandler doesn't know about headers anymore
api/client: configure api/client headers on Endpoint and use it to make requests
stats/client: configure api/client headers on Endpoint and use it to make requests

fbshipit-source-id: c917cbb

client/client_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -12,12 +13,13 @@ import (
 	"github.com/turbinelabs/api/fixture"
 	apihttp "github.com/turbinelabs/api/http"
 	"github.com/turbinelabs/api/http/envelope"
+	apiheader "github.com/turbinelabs/api/http/header"
 	"github.com/turbinelabs/api/service"
 	"github.com/turbinelabs/test/assert"
 )
 
 const (
-	clusterTestApiKey    = "whee-whee-whee"
+	clientTestApiKey     = "whee-whee-whee"
 	clusterCommonURL     = "/v1.0/cluster"
 	domainCommonURL      = "/v1.0/domain"
 	proxyCommonURL       = "/v1.0/proxy"
@@ -46,6 +48,32 @@ type verifyingHandler struct {
 func (w verifyingHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	rr := apihttp.NewRichRequest(r)
 	rrw := apihttp.RichResponseWriter{rw}
+
+	apiKey := rr.Underlying().Header.Get(http.CanonicalHeaderKey(apiheader.APIKey))
+	if apiKey != clientTestApiKey {
+		rw.WriteHeader(400)
+		rw.Write([]byte(
+			fmt.Sprintf(
+				"wrong api key header, got %s, want %s",
+				apiKey,
+				clientTestApiKey,
+			),
+		))
+		return
+	}
+
+	clientId := rr.Underlying().Header.Get(http.CanonicalHeaderKey(apiheader.ClientID))
+	if clientId != apiClientID {
+		rw.WriteHeader(400)
+		rw.Write([]byte(
+			fmt.Sprintf(
+				"wrong client id header: got %s, want %s",
+				clientId,
+				apiClientID,
+			),
+		))
+		return
+	}
 
 	w.fn(rr)
 
@@ -102,7 +130,7 @@ func newTestEndpointFromServer(server *httptest.Server) apihttp.Endpoint {
 
 func getAllInterface(server *httptest.Server) service.All {
 	endpoint := newTestEndpointFromServer(server)
-	serviceall, err := NewAll(endpoint, clusterTestApiKey, http.DefaultClient)
+	serviceall, err := NewAll(endpoint, clientTestApiKey)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -111,7 +139,7 @@ func getAllInterface(server *httptest.Server) service.All {
 
 func getAdminInterface(server *httptest.Server) service.Admin {
 	endpoint := newTestEndpointFromServer(server)
-	admin, err := NewAdmin(endpoint, clusterTestApiKey, http.DefaultClient)
+	admin, err := NewAdmin(endpoint, clientTestApiKey)
 	if err != nil {
 		log.Fatal(err)
 	}

turbinelabs/stats

client/client_test.go
@@ -18,6 +18,7 @@ import (
 	apihttp "github.com/turbinelabs/api/http"
 	"github.com/turbinelabs/api/http/envelope"
 	httperr "github.com/turbinelabs/api/http/error"
+	apiheader "github.com/turbinelabs/api/http/header"
 	statsapi "github.com/turbinelabs/api/service/stats"
 	"github.com/turbinelabs/logparser/metric"
 	"github.com/turbinelabs/nonstdlib/executor"
@@ -60,7 +61,7 @@ var (
 		},
 	}
 
-	apiKey = "i.am.a.key"
+	testApiKey = "i.am.a.key"
 
 	endpoint, _ = apihttp.NewEndpoint(apihttp.HTTP, "example.com", 8080)
 )
@@ -99,7 +100,6 @@ type requestFunc func(statsapi.StatsService) (*statsapi.ForwardResult, error)
 type newStatsFunc func(
 	apihttp.Endpoint,
 	string,
-	*http.Client,
 	executor.Executor,
 ) (statsapi.StatsService, error)
 
@@ -123,7 +123,7 @@ func prepareStatsClientTest(
 			},
 		)
 
-	client, err := NewStatsClient(e, apiKey, &http.Client{}, mockExec)
+	client, err := NewStatsClient(e, testApiKey, mockExec)
 	assert.Nil(t, err)
 
 	rvChan := make(chan forwardResult, 1)
@@ -181,6 +181,32 @@ type testHandler struct {
 }
 
 func (h *testHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	apiKey := req.Header.Get(http.CanonicalHeaderKey(apiheader.APIKey))
+	if apiKey != testApiKey {
+		resp.WriteHeader(400)
+		resp.Write([]byte(
+			fmt.Sprintf(
+				"wrong api key header, got %s, want %s",
+				apiKey,
+				testApiKey,
+			),
+		))
+		return
+	}
+
+	clientId := req.Header.Get(http.CanonicalHeaderKey(apiheader.ClientID))
+	if clientId != statsClientID {
+		resp.WriteHeader(400)
+		resp.Write([]byte(
+			fmt.Sprintf(
+				"wrong client id header: got %s, want %s",
+				clientId,
+				statsClientID,
+			),
+		))
+		return
+	}
+
 	body := req.Body
 	assert.NonNil(h.t, body)
 